243ce05382915c285566297987aca0348749823a
obolos <mycontaktperson@gmail.com>
2020-03-11 23:59:27 +0300

    Updates

--- README.md
+++
@@ -1,5 +0,0 @@
-git clone https://github.com/Rob--W/cors-anywhere.git
-cd cors-anywhere/
-npm install
-heroku create
-git push heroku master
---
+++ precache-manifest.d08988bfb5de6a5d7fe3e385bc8a75e5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "30895ef333f2bdfd2b852f4515626759",
+    "url": "/search-project-ui/index.html"
+  },
+  {
+    "revision": "1297efae9d3fd479f021",
+    "url": "/search-project-ui/static/css/main.38ed0210.chunk.css"
+  },
+  {
+    "revision": "e8076be814fc70906a65",
+    "url": "/search-project-ui/static/js/2.fa62b020.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/search-project-ui/static/js/2.fa62b020.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1297efae9d3fd479f021",
+    "url": "/search-project-ui/static/js/main.eb6f9caf.chunk.js"
+  },
+  {
+    "revision": "8538117f4eb3131dced1",
+    "url": "/search-project-ui/static/js/runtime-main.9236936d.js"
+  }
+]);
